dda1fb74eb72e7fc4a2c120c4a9550a14e7f80e3
Alejandro Gálvez <alex3galvez@gmail.com>
2023-06-10 20:43:04 +0200

    GCP: Improved jumpbox module readability

--- basic-arch/gcp/6-approach/terraform/modules/jumpbox/main.tf
+++ basic-arch/gcp/6-approach/terraform/modules/jumpbox/main.tf
@@ -1,14 +1,13 @@
 
 resource "google_compute_instance" "jumpbox_host" {
   name         = "my-jumpbox-host"
-  machine_type = "e2-micro"
 
+  machine_type = "e2-micro"
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-1804-lts"
     }
   }
-
   network_interface {
     subnetwork = var.subnetwork
     access_config {
