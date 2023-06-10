f3c09bc8471efe2420f9f6cfde8658ba05bbde9e
Alejandro Gálvez <alex3galvez@gmail.com>
2023-06-10 18:14:00 +0200

    AWS: Minor changes in jumpbox module disposition

--- basic-arch/aws/5-approach/terraform/modules/jumpbox/main.tf
+++ basic-arch/aws/5-approach/terraform/modules/jumpbox/main.tf
@@ -1,3 +1,26 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
+
+data "aws_ec2_instance_types" "free_instance" {
+  filter {
+    name   = "free-tier-eligible"
+    values = ["true"]
+  }
+}
+
 resource "aws_security_group" "allow_ssh" {
   description = "Allow SSH to the jumpbox"
   vpc_id      = var.vpc_id
@@ -29,40 +52,21 @@ resource "aws_key_pair" "this" {
   public_key = file("${path.cwd}/../../ssh-keys/test_jumpbox.pub")
 }
 
-resource "aws_instance" "jumpbox" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = data.aws_ec2_instance_types.free_instance.instance_types[0]
+resource "aws_network_interface" "this" {
+  subnet_id       = var.jumpbox_subnet
+  security_groups = [aws_security_group.allow_ssh.id]
+}
 
+resource "aws_instance" "jumpbox" {
+  instance_type = data.aws_ec2_instance_types.free_instance.instance_types[0]
+  ami           = data.aws_ami.ubuntu.id
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.this.id
+  }
   key_name                    = aws_key_pair.this.key_name
-  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
-  subnet_id                   = var.jumpbox_subnet
-  associate_public_ip_address = true
 
   tags = {
     name = "myJumpbox"
-  }
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-}
-
-data "aws_ec2_instance_types" "free_instance" {
-
-  filter {
-    name   = "free-tier-eligible"
-    values = ["true"]
   }
 }
